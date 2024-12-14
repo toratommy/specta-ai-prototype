21613ddf70cf17cd1e630ead4856782d7dbb7c65
toratommy <tommy@torainsights.com>
2024-12-13 18:49:39 -0600

    move login from sidebar to dialog window

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def login_dialog():
             st.session_state.logged_in = True
             st.session_state.username = username
             st.success("Login successful!")
-            st.experimental_rerun()
+            st.rerun()
         else:
             st.error("Invalid credentials.")
 
